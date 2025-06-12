9e202550f164b77b8daae3739287a878557cbc58
sunjingyu <sunjingyu@impr-inc.com>
2025-06-12 18:30:09 +0800

    delete：删除无用资源

--- dist/components/d-icon.js
+++
@@ -1,9 +0,0 @@
-import { D as DIcon$1, d as defineCustomElement$1 } from './p-B3vhzd0Y.js';
-
-const DIcon = DIcon$1;
-const defineCustomElement = defineCustomElement$1;
-
-export { DIcon, defineCustomElement };
-//# sourceMappingURL=d-icon.js.map
-
-//# sourceMappingURL=d-icon.js.map
